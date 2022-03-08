f6364f72b72eba0b2c4ab6a0080d77f7eeb45851
Mallory <mallorydoescoding@gmail.com>
2022-03-07 20:41:05 -0600

    rendered both components in the app component

--- the-teapot/src/App.js
+++ the-teapot/src/App.js
@@ -1,14 +1,17 @@
 //Component Imports
 import Header from '/components/Header.js'
+import Navbar from '/components/Navbar.js'
 
 //Style imports
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="mainContainer">
       <div className="innerContainer">
         <Header />
+        <Navbar />
       </div>
     </div>
   );
